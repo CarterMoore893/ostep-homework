7436ef3bc3e987b81fa1fe871957068b2e70f9b0
Carter Moore <cartermoore893@gmail.com>
2023-11-04 21:02:21 -0700

    Add comment on my guess to the problem's question

--- ch05-process-API/02.c
+++ ch05-process-API/02.c
@@ -14,9 +14,18 @@ Task: Write a program that opens a file (with the open() system call)
 #include <fcntl.h>
 #include <sys/wait.h>
 
+/* I read the man pages for fork, which stated "The child inherits copies
+   of the parent's set of open file descriptors **and description**
+   This means that the two file descriptors share open file **status flags,
+   file offset, and signal-driven I/O attributes**"
+   
+   This tells me that both the parent and child will have the fd open,
+   but I don't think they will be able to write at the same time. At best,
+   the scheduler will just pick a line to write first at worst, it'll crash.
+*/
+
 int main(int argc, char *argv[])
 {
-
     int rc = fork();
     // On a fork, the PID of the child is returned to the parent,
     // and `0` is returned in the child.
