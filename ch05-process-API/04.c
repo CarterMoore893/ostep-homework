5370b012af58ed5a521c7f017bf5eb152fd21f6e
Carter Moore <cartermoore893@gmail.com>
2023-11-12 15:11:33 -0800

    Perform all forms of exec()

--- ch05-process-API/04.c
+++ ch05-process-API/04.c
@@ -5,6 +5,7 @@ exec() to run the program /bin/ls. See if you can try all of the
 variants of exec(), including (on Linux) execl(), execle(),
 execlp(), execv(), execvp(), and execvpe(). Why do
 you think there are so many variants of the same basic call?*/
+#define _GNU_SOURCE // Needed for execvpe
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,21 +16,103 @@ you think there are so many variants of the same basic call?*/
 
 int main(int argc, char *argv[])
 {
-    printf("Hello from pre-fork!\n");
-
+    // execl
+    printf("(%d) Parent started\n", getpid());
     int rc = fork();
-    printf("Hello from post-fork! I am %d\n", getpid());
-    // On a fork, the PID of the child is returned to the parent,
-    // and `0` is returned in the child.
-    // On failure, -1 is returned in the parent and no child is created.
     if (rc == -1) {
         fprintf(stderr, "(EE) %d fork failed\n", getpid());
         exit(EXIT_FAILURE);
     } else if (rc == 0) {
         // Child process executes here
-        scanf("What do you want to say?");
-        printf("Child %d->%d says hello!\n", getppid(), getpid());
-    }    return EXIT_SUCCESS;
+        char* my_args[3];
+        my_args[0] = "/bin/ls";
+        my_args[1] = "-l";
+        printf("(%d) execl\n", getpid());
+        execl(my_args[0], my_args[1], NULL);
+        // Nothing beyond this line will execute for this Child
+        printf("I will not execute!\n");
+    }
+    wait(&rc);
+
+    // execlp
+    rc = fork();
+    if (rc == -1) {
+        fprintf(stderr, "(EE) %d fork failed\n", getpid());
+        exit(EXIT_FAILURE);
+    } else if (rc == 0) {
+        // Child process executes here
+        char* my_args[3];
+        my_args[0] = strdup("ls");
+        my_args[1] = strdup("");
+        printf("(%d) execlp\n", getpid());
+        execlp(my_args[0], my_args[1], NULL);
+    }
+    wait(&rc);
+
+    // execle
+    rc = fork();
+    if (rc == -1) {
+        fprintf(stderr, "(EE) %d fork failed\n", getpid());
+        exit(EXIT_FAILURE);
+    } else if (rc == 0) {
+        // Child process executes here
+        char* my_args[3];
+        my_args[0] = "/usr/bin/ls";
+        my_args[1] = "l";
+        char* my_env[2];
+        my_env[0] = "/usr/bin";
+        printf("(%d) execle\n", getpid());
+        execle(my_args[0], my_args[1], NULL, my_env);
+    }
+    wait(&rc);
+
+    // execv
+    rc = fork();
+    if (rc == -1) {
+        fprintf(stderr, "(EE) %d fork failed\n", getpid());
+        exit(EXIT_FAILURE);
+    } else if (rc == 0) {
+        // Child process executes here
+        char* my_args[3];
+        my_args[0] = "/usr/bin/ls";
+        printf("(%d) execv\n", getpid());
+        execv(my_args[0], my_args);
+    }
+    wait(&rc);
+
+    // execvp
+    rc = fork();
+    if (rc == -1) {
+        fprintf(stderr, "(EE) %d fork failed\n", getpid());
+        exit(EXIT_FAILURE);
+    } else if (rc == 0) {
+        // Child process executes here
+        char* my_args[3];
+        my_args[0] = "ls";
+        printf("(%d) execvp\n", getpid());
+        execvp(my_args[0], my_args);
+    }
+    wait(&rc);
+
+    // execvpe
+    rc = fork();
+    if (rc == -1) {
+        fprintf(stderr, "(EE) %d fork failed\n", getpid());
+        exit(EXIT_FAILURE);
+    } else if (rc == 0) {
+        // Child process executes here
+        char* my_args[3];
+        char* my_environ[2];
+        my_args[0] = "ls";
+        my_args[1] = "-l";
+        my_environ[0] = "/usr/bin";
+        my_environ[1] = "/bin";
+        printf("(%d) execvpe\n", getpid());
+        execvpe(my_args[0], my_args, my_environ);
+    }
+    wait(&rc);
+    printf("(%d) Done\n", getpid());
+    return EXIT_SUCCESS;
 }
 /* Answers:
 */
